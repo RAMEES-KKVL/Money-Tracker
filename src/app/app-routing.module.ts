23277d11812f2b0741bdbb20feb72cb424d8789b
RAMEES-KKVL <rameesp41750@gmail.com>
2024-08-31 12:46:08 +0530

    Completed header component UI design"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageBody } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "",
+    component : HomepageBody
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageBody } from './home/home.component';
+import { HeaderComponent } from './home/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageBody,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
