8f1fe75f57feb36f9c48d0dc2c395769aeba4e9c
RAMEES-KKVL <rameesp41750@gmail.com>
2024-09-15 19:33:08 +0530

    Completed UI design of form for adding new transaction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageBody } from './home/home.component';
 import { HeaderComponent } from './home/components/header/header.component';
+import { SidebarComponent } from './home/components/sidebar/sidebar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageBody,
     HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/components/header/header.component.ts
+++ src/app/home/components/header/header.component.ts
@@ -1,7 +1,27 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector : "header-component",
     templateUrl : "./header.component.html"
 })
-export class HeaderComponent {}
+export class HeaderComponent { 
+    togglePopup: boolean = false
+    transactionForm: FormGroup
+    constructor(private fb: FormBuilder) {
+        this.transactionForm = this.fb.group({
+            transactionType: ['', Validators.required]
+        })
+    }
+    addTransaction() {
+        this.togglePopup = !this.togglePopup
+    }
+    categoryType: boolean = false
+
+    checkingType() {
+        console.log(1);
+        
+    }
+
+    
+}
